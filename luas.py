eb55a5b2f859a921dc88e34ba0588a67daa52fe1
Dinda Nurafni <90329718+DindaNurafniUPI@users.noreply.github.com>
2021-09-12 14:28:30 +0700

    Add files via upload

---
+++ luas.py
@@ -0,0 +1,7 @@
+def luas_persegi(sisi):
+    luas_persegi = sisi ** 2
+    return luas_persegi
+
+def luas_persegi_panjang(panjang, lebar):
+    luas_persegi_panjang = panjang * lebar
+    return luas_persegi_panjang
